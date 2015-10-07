72eee2692febf6fa98bf7f759eedc8df0bbed214
Julian Fleischer <julian.fleischer@fu-berlin.de>
2015-10-08 00:10:36 +0200

    Added (yet empty) test suites

---
+++ test/coll/base/lookup.js
@@ -0,0 +1,10 @@
+/* vim: set et sw=2 ts=2: */
+
+describe('', function () {
+  'use strict';
+
+  var assert = require('../../../util/assert.js');
+  var Nodash = require('../../../nodash');
+
+});
+

96b2dc3adb6f72e442caa24db78247e9a2a2f16d
Julian Fleischer <julian.fleischer@fu-berlin.de>
2015-10-08 00:48:57 +0200

    test coverage, sweet test coverage

--- test/coll/base/length.js
+++ test/coll/base/length.js
@@ -6,5 +6,23 @@ describe('', function () {
   var assert = require('../../../util/assert.js');
   var Nodash = require('../../../nodash');
 
+  var length = Nodash.length;
+  var lazy   = Nodash.lazy;
+  var repeat = Nodash.repeat;
+
+  it('length reports Infinity for streams', function () {
+    assert.strictEqual(Infinity, length(repeat('x')));
+  });
+
+  it('length reports correct length for lists', function () {
+    assert.strictEqual(7, length(range(1, 7)));
+  });
+
+  it('throws if neither array, string, list, stream, nor object is passed', function () {
+    assert.throws(function () {
+      length(17);
+    });
+  });
+  
 });
 
--- test/coll/base/take.js
+++ test/coll/base/take.js
@@ -1,10 +1,36 @@
 /* vim: set et sw=2 ts=2: */
 
-describe('', function () {
+describe('take', function () {
   'use strict';
 
   var assert = require('../../../util/assert.js');
   var Nodash = require('../../../nodash');
+
+  var take   = Nodash.take;
+  var repeat = Nodash.repeat;
+  var range  = Nodash.range;
+  var lazy   = Nodash.lazy;
+  var listToArray = Nodash.listToArray;
+
+  it('should take 3 elements from an array', function () {
+    assert.deepEqual([1, 2, 3], take(3, [1, 2, 3, 4, 5]));
+  });
+
+  it('should take 3 elements from a string', function () {
+    assert.strictEqual("abc", take(3, "abcdef"));
+  });
+
+  it('should take 3 elements from a list', function () {
+    assert.deepEqual([1, 2, 3], listToArray(take(3, lazy([1, 2, 3, 4, 5]))));
+  });
+
+  it('should take 3 elements from a stream', function () {
+    assert.deepEqual([1, 1, 1], listToArray(take(3, repeat(1))));
+  });
+
+  it('should take 3 elements from a range', function () {
+    assert.deepEqual([1, 2, 3], listToArray(take(3, range(1, 5))));
+  });
 
 });
 

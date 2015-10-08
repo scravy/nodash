a9cc3cba1a635b158869ea38a8740e9c2f4b8cd9
Julian Fleischer <tirednesscankill@warhog.net>
2015-10-08 13:19:13 +0200

    last /w stream

--- test/coll/base/last.js
+++ test/coll/base/last.js
@@ -6,5 +6,26 @@ describe('', function () {
   var assert = require('../../../util/assert.js');
   var Nodash = require('../../../nodash');
 
+  var last = Nodash.last;
+  var lazy = Nodash.lazy;
+  var repeat = Nodash.repeat;
+
+  it('should extract last element from a string', function () {
+    assert.strictEqual('c', last('abc'));
+  });
+
+  it('should extract last element from an array', function () {
+    assert.strictEqual(3, last([1, 2, 3]));
+  });
+
+  it('should extract last element from a list', function () {
+    assert.strictEqual(3, last(lazy([1, 2, 3])));
+  });
+
+  it('should throw in case a stream is passed', function () {
+    assert.throws(function () {
+      last(repeat(3));
+    });
+  });
 });
 
